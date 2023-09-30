573a6f8afc83a1aa3b9e5e2e0081f5497c4adac7
= <=>
2023-09-30 17:01:41 +0300

    complete hw 20.1

--- config/settings.py
+++ config/settings.py
@@ -127,6 +127,7 @@ STATICFILES_DIRS = (
 	BASE_DIR / 'static',
 )
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
@@ -136,5 +137,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # My path 
 
 FORM_DATA_PATH = BASE_DIR / 'form_data.txt'
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
